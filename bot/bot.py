4edb139454035cbd7547dce68d76cc7f4b6192f0
ArtakOvakimyan <ovakimyanartak4@gmail.com>
2024-11-26 00:26:38 +0500

    removed test function

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,4 @@
 import os
-from handler.hello import hello
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from dotenv import load_dotenv
 
@@ -11,6 +10,5 @@ BOT_TOKEN = os.getenv('TOKEN')
 
 if __name__ == '__main__':
   app = ApplicationBuilder().token(BOT_TOKEN).build()
-  app.add_handler(CommandHandler("hello", hello))
   app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, process_image))
   app.run_polling()
