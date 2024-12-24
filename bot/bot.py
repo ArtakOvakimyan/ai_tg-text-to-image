37804bdae0b7b1492b53a755123445f4be39b3e6
ArtakOvakimyan <ovakimyanartak4@gmail.com>
2024-12-25 02:40:39 +0500

    tests

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,13 @@
 import os
+from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from dotenv import load_dotenv
-
-from handler.process_prompt import process_prompt
 
 load_dotenv()
 BOT_TOKEN = os.getenv('TOKEN')
 
 if __name__ == '__main__':
+    from handler.process_prompt import process_prompt
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, process_prompt))
-    app.run_polling()
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
